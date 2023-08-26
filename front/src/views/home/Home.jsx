67d70b851c2a6c32e64c1810ffaa89f6fb058b22
Jonathan Fernandez <arcancode@gmail.com>
2023-08-25 23:25:13 -0300

    server

---
+++ front/src/views/home/Home.jsx
@@ -0,0 +1,14 @@
+import h from './Home.module.css'
+
+
+const Home =()=>{
+    return(
+        <div className={h.home} >
+
+            <div>Homes</div>
+        </div>
+    )
+}
+
+
+export default Home
