0f8639980abf4f3a51728259383f4fd55008f373
andrea <andrea.buldorini2013@gmail.com>
2023-09-21 22:10:44 -0300

    user3 detalles de user creador

--- Client/src/redux/actionsType.js
+++ Client/src/redux/actionsType.js
@@ -8,4 +8,4 @@ export const GET_ORDER = "GET_ORDER"
 export const ORDER_DETAIL = "ORDER_DETAIL"
 export const CLEAN_DETAIL = "CLEAN_DETAIL"
 export const BY_TYPE = "BY_TYPE"
-
+export const GET_REVIEWS = "GET_REVIEWS"
